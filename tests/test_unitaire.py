70a14b155cb747050ce70714f4788c3bd63189ca
clementinesacre <sacre.clementine@gmail.com>
2020-12-12 16:17:43 +0100

    Test 9 de l'automatisation des tests.

--- tests/test_unitaire.py
+++ tests/test_unitaire.py
@@ -17,14 +17,14 @@ class CultureGeneraleTest(unittest.TestCase):
                                           'Quel chiffre est une puissance de 2 ?'], 1)), 1)
 
     def test_recup_donnees_fichier(self):
-        self.assertEqual(f.recup_donnees_fichier("../ressources/geographie.csv"),
+        """self.assertEqual(f.recup_donnees_fichier("../ressources/geographie.csv"),
                          [['Combien vaut 2 + 2 = ?', '4', '3', '8', '4', '12'],
                           ['Quelle est la capitale de la Belgique ?', 'Bruxelles', 'Bruxelles', 'Namur',
-                           'Ostende', 'Liege']])
-        self.assertEqual(f.recup_donnees_fichier("geographie.csv"),
+                           'Ostende', 'Liege']])"""
+        """self.assertEqual(f.recup_donnees_fichier("geographie.csv"),
                          [['Combien vaut 2 + 2 = ?', '4', '3', '8', '4', '12'],
                           ['Quelle est la capitale de la Belgique ?', 'Bruxelles', 'Bruxelles', 'Namur',
-                           'Ostende', 'Liege']])
+                           'Ostende', 'Liege']])"""
         self.assertEqual(f.recup_donnees_fichier("./ressources/geographie.csv"),
                          [['Combien vaut 2 + 2 = ?', '4', '3', '8', '4', '12'],
                           ['Quelle est la capitale de la Belgique ?', 'Bruxelles', 'Bruxelles', 'Namur',
