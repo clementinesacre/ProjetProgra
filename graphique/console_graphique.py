dc97a36aebfaee5a806549be8798b036167dac34
pouspous <bonnetcecile1460@gmail.com>
2020-12-16 09:02:27 +0100

    log graphique

--- graphique/console_graphique.py
+++ graphique/console_graphique.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-
+import logging
+logger = logging.getLogger("cultureg")
 
 class Graphique:
     def __init__(self, dictionnaire):
@@ -21,7 +22,9 @@ class Graphique:
                     maximum = len(self.__dic[i][j])
                     a_rajouter = len(self.__dic[i][j]) + 1
         self.__canv = tk.Canvas(self.__racine, bg="#E7EBD6", height=500, width=525 + a_rajouter * 17)
+        logger.info('graphique/console_graphique.py : __init__() : Création de tk.Canvas')
         self.__canv.pack()
+        logger.info('graphique/console_graphique.py : __init__() : Mise en place de tk.Canvas')
 
         self.__liste_joueur = []
         self.__liste_theme = []
@@ -31,8 +34,11 @@ class Graphique:
         self.__quitter = tk.Button(self.__racine, text="Back", height=2, width=15, bg='#5D5F56', fg='#ffffff', bd=1,
                                    activebackground='#FFFFFF', activeforeground='#CCCCFF', cursor='circle',
                                    command=lambda: self.revenir_menu())
+        logger.info('graphique/console_graphique.py : __init__() : Création de tk.Button quitter')
+
         self.__titre_joueur = tk.Label(self.__racine, text="Noms des joueurs :", bg='#E7EBD6', height=2, width=18,
                                        fg='#5D5F56', font=10)
+        logger.info('graphique/console_graphique.py : __init__() : Création de tk.Label titre_joueur')
 
         self.initialisation()
 
@@ -45,6 +51,7 @@ class Graphique:
         revenir en arrière, et le titre.
         """
         self.__titre_joueur.place(x=16, y=20)
+        logger.info('graphique/console_graphique.py : initialisation() : Placement de tk.Label titre_joueur')
 
         x_initial = 40
         y_initial = 80
@@ -54,7 +61,9 @@ class Graphique:
             button1 = tk.Button(self.__racine, text=joueur, height=2, width=15, bg='#86887F', fg='#ffffff', bd=1,
                                 activebackground='#FFFFFF', activeforeground='#CCCCFF', cursor='circle',
                                 command=lambda nom=joueur: self.matieres(nom))
+            logger.info('graphique/console_graphique.py : initialisation() : Création de tk.Button joueur : ' + joueur)
             button1.place(x=x_initial * compteur_x + 15, y=y_initial * compteur_y)
+            logger.info('graphique/console_graphique.py : initialisation() : Placement de tk.Button joueur : ' + joueur)
             self.__liste_joueur.append(button1)
             if (y_initial * compteur_y) >= 400:
                 compteur_x = 5
@@ -74,9 +83,12 @@ class Graphique:
         self.__joueur_courant = joueur
         for bouton in self.__liste_joueur:
             bouton.destroy()
+            logger.info('graphique/console_graphique.py : matieres() : Suppression de tk.Button joueurs')
         self.__titre_joueur.destroy()
+        logger.info('graphique/console_graphique.py : matieres() : Suppression de tk.Label titre_joueurs')
 
         self.__quitter.place(x=20, y=20)
+        logger.info('graphique/console_graphique.py : matieres() : Placement de tk.Button quitter')
 
         x_initial = 40
         y_initial = 80
@@ -87,7 +99,10 @@ class Graphique:
                 button2 = tk.Button(self.__racine, text=theme, height=2, width=15, bg='#86887F', fg='#ffffff', bd=1,
                                     activebackground='#FFFFFF', activeforeground='#CCCCFF', cursor='circle',
                                     command=lambda nom=theme: self.points(nom))
+                logger.info('graphique/console_graphique.py : matieres() : Création de tk.Button theme: ' + theme )
                 button2.place(x=x_initial * compteur_x + 17, y=y_initial * compteur_y)
+                logger.info('graphique/console_graphique.py : matieres() : Placement de tk.Button theme: ' + theme )
+
                 self.__liste_theme.append(button2)
                 if (y_initial * compteur_y) >= 400:
                     compteur_x = 5
@@ -104,8 +119,10 @@ class Graphique:
         """
         for bouton in self.__liste_theme:
             bouton.destroy()
+            logger.info('graphique/console_graphique.py : points() : Suppression de tk.Button themes')
 
         self.__quitter.place(x=20, y=20)
+        logger.info('graphique/console_graphique.py : points() : Placement de tk.Button quitter')
 
         x_initial = 40
         y_initial = 80
@@ -115,7 +132,9 @@ class Graphique:
         for score in self.__dic[self.__joueur_courant][theme]:
             label = tk.Label(self.__racine, text=(str(score[1]) + " : " + str(score[0]) + "%"), bg='#FF9966', height=2,
                              width=17, fg='#000000', relief="sunken")
+            logger.info('graphique/console_graphique.py : points() : Création de tk.Label score: ' + str(score))
             label.place(x=x_initial * compteur_x + 17, y=y_initial * compteur_y)
+            logger.info('graphique/console_graphique.py : points() : Placement de tk.Label score: ' + str(score))
             if (y_initial * compteur_y) > base - 50:
                 compteur_x += 4
                 compteur_y = 0
@@ -132,13 +151,23 @@ class Graphique:
         """
         for points in self.__liste_score:
             points.destroy()
+            logger.info('graphique/console_graphique.py : revenir_menu() : Suppression de tk.Label scores')
+
         for theme in self.__liste_theme:
             theme.destroy()
+            logger.info('graphique/console_graphique.py : revenir_menu() : Suppression de tk.Button theme')
+
         self.__quitter.destroy()
+        logger.info('graphique/console_graphique.py : revenir_menu() : Suppression de tk.Button quitter')
+
         self.__quitter = tk.Button(self.__racine, text="Back", height=2, width=15, bg='#5D5F56', fg='#ffffff', bd=1,
                                    activebackground='#FFFFFF', activeforeground='#CCCCFF', cursor='circle',
                                    command=lambda: self.revenir_menu())
+        logger.info('graphique/console_graphique.py : revenir_menu() : Création de tk.Button quitter')
+
         self.__titre_joueur = tk.Label(self.__racine, text="Noms des joueurs :", bg='#E7EBD6', height=2, width=18,
                                        fg='#5D5F56', font=10)
+        logger.info('graphique/console_graphique.py : revenir_menu() : Création de tk.Label titre_joueur')
+
         self.initialisation()
 
